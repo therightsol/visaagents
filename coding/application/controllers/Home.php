d6bd9c972fc726895d76970de8251f89acd8b974
Ali Shan <alishan@therightsol.com>
2016-01-27 23:30:02 +0500

    just test

--- coding/application/controllers/Home.php
+++ coding/application/controllers/Home.php
@@ -4,6 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Home extends CI_Controller {
     public function index(){
         $data['pagename'] = 'home';
+        
         $this->load->view('home' , $data);
     }
 }
